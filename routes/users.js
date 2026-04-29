ff5d53a8bd4598a8c69d8adedf89b22d1be573fe
Jessica Organ <jorgan612@gmail.com>
2026-04-29 14:11:45 -0600

    Added additional anticipated http methods to users file.

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,23 @@ router.get('/', (req, res) => {
     res.send('GET USERS LIST');
 });
 
-
 router.get('/new', (req, res) => {
     res.send("NEW USER FORM");
+}); 
+
+router.post('/', (req, res) => {
+    res.send('Create User')
+});
+
+router.route('/:id')
+.get((req, res) => {
+    res.send(`Get User with ID ${req.params.id}`);
+})
+.put((req, res) => {
+    res.send(`Post new user with ID ${req.params.id}`);
+})
+.delete((req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`);
 });
 
  module.exports = router;
