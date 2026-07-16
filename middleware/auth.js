c278ca11e553b4af205a07f477c51d6eff210eb9
Jessica Organ <jorgan612@gmail.com>
2026-07-16 11:29:13 -0600

    Fixed typo in auth.js message.

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const verifyToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
 
     if (!token) {
-        return res.status(403).json({ message: "A toke is required for authorization" });
+        return res.status(403).json({ message: "A token is required for authorization" });
     }
 
     try {
